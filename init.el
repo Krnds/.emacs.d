05ee08be4cdc1ae2bedab48b956fa41f648b8c68
Anthony Le Cigne <dev@lecigne.net>
2024-07-21 12:33:48 +0200

    Add hook to page-break-lines-mode

--- init.el
+++ init.el
@@ -230,7 +230,8 @@
   ;; characters as tidy horizontal rules.
   ;; https://github.com/purcell/page-break-lines
   :ensure t
-  :delight)
+  :delight
+  :hook (dashboard-mode . page-break-lines-mode))
 
 ;; * Shortcut for inserting today's date
 ;(defun insert-todays-date (arg)
